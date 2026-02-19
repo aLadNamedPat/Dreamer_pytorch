101f996cdb088f5f086e43111abe6a33cdaa0e0a
aLadNamedPat <Pattheboy135@gmail.com>
2026-02-19 07:37:46 -0500

    removed actor bottleneck

--- models/action_value.py
+++ models/action_value.py
@@ -4,16 +4,16 @@ import torch.nn.functional as F
 
 
 class Action(nn.Module):
-    def __init__(self, imagined_state_dim, output_dim, hidden_dim = [300, 300]):
+    def __init__(self, imagined_state_dim, output_dim, hidden_dim = [300, 300, 300]):
         super().__init__()
         # the action model takes as input the current imagined state and returns a mean of what the
         # action should be as well as a standard devaition. (i.e. the action returns a gaussian)
         self.layer_1 = nn.Linear(imagined_state_dim, hidden_dim[0])
         self.layer_2 = nn.Linear(hidden_dim[0], hidden_dim[1])
-        self.layer_3 = nn.Linear(hidden_dim[1], output_dim)
+        self.layer_3 = nn.Linear(hidden_dim[1], hidden_dim[2])
 
-        self.mu = nn.Linear(output_dim, output_dim)
-        self.std = nn.Linear(output_dim, output_dim)
+        self.mu = nn.Linear(hidden_dim[2], output_dim)
+        self.std = nn.Linear(hidden_dim[2], output_dim)
 
     def forward(self, x):
         x = F.elu(self.layer_1(x))
